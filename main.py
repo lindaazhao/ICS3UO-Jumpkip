1b09f8c2e81f12c253649b4977792d59f6345f3d
lindaazhao <linda.zhao23@gmail.com>
2020-06-18 19:10:46 -0400

    June 17 - obstacle graphics & animation

--- main.py
+++ main.py
@@ -1,26 +1,54 @@
+# ICS3UO - Pygame Assignment
+# Student: Linda Zhao
+
 import pygame
 import random
-
-# Things I still need to add:
-# allll the graphics (tree stump, rocks)
-# random obstacle generation w/ different hitboxes
-
-# --- Stuff that's DONE! ---
-# obstacle collision (end game when main character hits an obstacle)
-# display scoretracker that starts counting once you start
-# increase in xVel as the score gets higher (every 1000 or something)
-# instructions on how to play the game
-# call it jumpkip lmfao
-# game over end screen & way to restart game
-
 
 # initialize display
 pygame.init()
 screensize = screenwidth, screenheight = 640, 480
 screen = pygame.display.set_mode(screensize)
 
+
+# function used to display text onto game screen
+# modified from the example on the following website: https://pythonprogramming.net/displaying-text-pygame-screen/
+def write(text, size, colour, xpos, ypos):
+    font = pygame.font.SysFont("consolas", size)
+    texttoscreen = font.render(text, False, colour)
+    screen.blit(texttoscreen, (xpos, ypos))
+
+
+# function to update the game screen - all drawing except for obstacle
+def redrawGameScreen():
+    screen.blit(background, (0, 0))  # draw in the background
+
+    # display instructions to play, static character
+    if introScreen:
+        screen.blit(char, (int(x), int(y)))
+        write("Welcome to Jumpkip!", 30, black, 160, 130)
+        write("Press the space key or up arrow key to jump", 20, black, 80, 170)
+        write("and the down arrow key to fall back down.", 20, black, 90, 195)
+
+    # used to animate the mudkip when the game has started
+    if gameStart:
+        global animCount
+        if animCount + 1 >= 20:
+            animCount = 0
+        screen.blit(mudkipAnim[animCount//5], (int(x), int(y)))
+        animCount += 1
+
+    # display game over instructions, static character
+    if gameOver:
+        screen.blit(char, (int(x), int(y)))
+        write("Game Over!", 40, black, 200, 150)
+        write("Jump to restart the game.", 20, black, 175, 200)
+
+    # write score to the screen
+    write("Score: " + str(int(scoreTracker)), 30, white, 50, 50)
+
+
 # ----- Game Elements -----
-ground = 300 # y value of the ground
+ground = 300  # y value of the ground
 scoreTracker = 0
 running = True
 gameStart = False
@@ -28,74 +56,70 @@ gameOver = False
 isJump = False
 cancelJump = False
 introScreen = True
+clock = pygame.time.Clock()
 lastFrame = pygame.time.get_ticks()  # get time in milliseconds since pygame.init()
+background = pygame.transform.scale(pygame.image.load('img/bg.png'), (700, 480))
+# colours
 white = (255, 255, 255)
 black = (0, 0, 0)
-background = pygame.transform.scale(pygame.image.load('img/bg.png'), (700, 480))
-
-
-# function used to display text onto game screen
-def write(text, size, colour, xpos, ypos):
-    font = pygame.font.SysFont("consolas", size)
-    texttoscreen = font.render(text, False, colour)
-    screen.blit(texttoscreen, (xpos, ypos))
-
-
-# function to update the game screen - has all drawing in it
-def redrawGameScreen():
-    screen.blit(background, (0, 0))
-
-    if introScreen:
-        screen.blit(char, (int(x), int(y)))
-        write("Welcome to Jumpkip!", 30, black, 160, 130)
-        write("Press the space key or up arrow key to jump", 20, black, 80, 170)
-        write("and the down arrow key to fall back down.", 20, black, 90, 195)
-
-    if gameStart:
-        screen.blit(mudkipAnim[1], (int(x), int(y)))
-
-    if gameOver:
-        screen.blit(char, (int(x), int(y)))
-        write("Game Over!", 40, black, 200, 150)
-        write("Jump to restart the game.", 20, black, 175, 200)
-
-    write("Score: " + str(int(scoreTracker)), 30, white, 50, 50)
-
+# sound effects
+jumpSound = pygame.mixer.Sound('jumpSound.wav')
+playJumpSound = True
+deathSound = pygame.mixer.Sound('deathSound.wav')
 
 # ----- Main Character -----
-x = 40
-y = 250
-width = 40
+x = 40  # starting x position
+y = 250  # starting y position
+width = 50
 height = 50
-yVel = 750  # initial y velocity of main character for jumping
+animCount = 0  # used to animate the main character
+yVel = 650  # initial y velocity of main character for jumping
 mudkipAnim = [pygame.transform.scale(pygame.image.load('img/mudkip1.png'), (50, 50)),
               pygame.transform.scale(pygame.image.load('img/mudkip2.png'), (50, 50)),
               pygame.transform.scale(pygame.image.load('img/mudkip1.png'), (50, 50)),
               pygame.transform.scale(pygame.image.load('img/mudkip2.png'), (50, 50))]
 char = pygame.transform.scale(pygame.image.load('img/mudkip1.png'), (50, 50))
-animCount = 0
 
 
 # ----- Obstacles -----
+# Obstacle class used to generate multiple instances of obstacles
+# Used to set obstacle width, height, x and y positions, and also includes function to draw the obstacle
 class Obstacle:
-    def __init__(self, size, groundY):
-        self.size = size
+    def __init__(self, width, height, groundY):
+        self.width = width
+        self.height = height
         self.groundY = groundY
         self.x = 640
 
-    def draw(self, display):
-        pygame.draw.rect(display, (255, 0, 0), [int(self.x), int(self.groundY - self.size), self.size, self.size])
+    def draw(self):
+        if self.height == 23:
+            obsType = obsGraphics[0]
+        elif self.height == 14:
+            obsType = obsGraphics[1]
+        elif self.height == 27:
+            obsType = obsGraphics[2]
+        else:
+            obsType = obsGraphics[3]
+        screen.blit(obsType, (int(self.x), int(self.groundY - self.height)))
 
 
-xVel = 150  # starting speed of obstacles
+xVel = 200  # starting speed of obstacles
 obstacle_list = []  # maintain a list of all obstacles that are currently on screen (active)
-xDiff = random.randint(120, 400)
-newObstacle = Obstacle(25, ground)
+obsGraphics = [pygame.transform.scale(pygame.image.load('img/obs1.png'), (35, 23)),
+               pygame.transform.scale(pygame.image.load('img/obs2.png'), (25, 14)),
+               pygame.transform.scale(pygame.image.load('img/obs3.png'), (35, 27)),
+               pygame.transform.scale(pygame.image.load('img/obs4.png'), (35, 35))]
+obsWidths = [35, 25, 35, 35]
+obsHeights = [23, 14, 27, 35]
+obsSize = random.randint(0, 3)
+xDiff = random.randint(150, 500)
+newObstacle = Obstacle(obsWidths[obsSize], obsHeights[obsSize], ground)
 obstacle_list.append(newObstacle)
 
 
 # ----- Main Game Loop -----
 while running:
+    clock.tick(20)
     time = pygame.time.get_ticks()  # get time in milliseconds since pygame.init()
     deltaTime = (time - lastFrame)/1000.0
     lastFrame = time
@@ -108,16 +132,18 @@ while running:
             if event.key == pygame.K_UP or event.key == pygame.K_SPACE:  # make character jump
                 if gameOver:
                     # reset all game variables to start a new game
-                    yVel = 750
-                    xVel = 150
+                    yVel = 650
+                    xVel = 200
                     scoreTracker = 0
                     obstacle_list = []
-                    xDiff = random.randint(120, 400)
-                    newObstacle = Obstacle(25, ground)
+                    xDiff = random.randint(150, 500)
+                    obsSize = random.randint(0, 3)
+                    newObstacle = Obstacle(obsWidths[obsSize], obsHeights[obsSize], ground)
                     obstacle_list.append(newObstacle)
                     gameOver = False
                     isJump = True
                     gameStart = True
+                    playJumpSound = True
                     break
                 isJump = True
                 gameStart = True  # game starts once user jumps for the first time
@@ -131,28 +157,30 @@ while running:
         y -= yVel * deltaTime
         if y > ground-height:
             isJump = False
-            yVel = 750
+            yVel = 650
             y = 250
+        if playJumpSound:
+            jumpSound.play()
+            playJumpSound = False
 
     if cancelJump:
         yVel += -6000 * deltaTime
         y -= yVel * deltaTime
         if y > ground-height:
             cancelJump = False
-            yVel = 750
+            yVel = 650
             y = 250
 
     if gameStart:  # obstacles start rolling
-        print("Game Started")
+        playDeathSound = True
         introScreen = False
         for obs in obstacle_list:
             obs.x -= xVel * deltaTime
             for i in range(x, x+width):
                 for j in range(int(y), int(y+height)):
-                    if i in range(int(obs.x), int(obs.x+obs.size)) and j in range(obs.groundY-obs.size, obs.groundY):
+                    if i in range(int(obs.x), int(obs.x+obs.width)) and j in range(obs.groundY-obs.height, obs.groundY):
                         gameOver = True
         scoreTracker += 30 * deltaTime
-        # print(int(scoreTracker)) TEST
         # increase speed of obstacles by 2 for every 100 points
         if int(scoreTracker) >= 100 and (int(scoreTracker) % 100) == 0:
             xVel += 3
@@ -160,12 +188,16 @@ while running:
         if int(scoreTracker) >= 1000 and (int(scoreTracker) % 1000) == 0:
             xVel += 30
             scoreTracker += 1  # prevents xVel from increase multiple times until scoreTracker increases
+        if y == ground-height:
+            playJumpSound = True
 
     if gameOver:  # if main character hits an obstacle
-        print("Game Over")
         gameStart = False
         isJump = False
         cancelJump = False
+        if playDeathSound:
+            deathSound.play()
+            playDeathSound = False
         yVel = 0
         xVel = 0
 
@@ -176,26 +208,22 @@ while running:
         obs = obstacle_list[i]  # set current obstacle
         # Test if obs is last obstacle
         if i == len(obstacle_list)-1:
-            obs.draw(screen)
-            # print(xDiff) TEST
+            obs.draw()
             # Test if current obstacle satisfies conditions for new obstacle generation
             # Condition: current obstacle must be xDiff away from edge of screen where a new obstacle is generated
             if obs.x < (640 - xDiff):
-                # print(obs.x) TEST
                 # Generate a new obstacle and add it to the list of active obstacles
-                newObstacle = Obstacle(25, ground)
+                obsSize = random.randint(0, 3)
+                newObstacle = Obstacle(obsWidths[obsSize], obsHeights[obsSize], ground)
                 obstacle_list.append(newObstacle)
                 # Generate a random number for the distance between last obstacle and new obstacle to be generated
                 xDiff = random.randint(150, 500)
         # If obstacle is off screen, remove it from the list of active obstacles
-        if obs.x < 0-obs.size:
+        if obs.x < 0-obs.width:
             obstacle_list.remove(obs)
             break
         else:
-            obs.draw(screen)
-
-    # if introScreen: TEST
-    #     print("Intro Screen") TEST
+            obs.draw()
 
     # Update the display
     pygame.display.flip()
